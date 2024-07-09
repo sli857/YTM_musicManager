4cfa8e8c8f0e2f0e90e9683b2aada09eeefd5764
Legion <sli857@wisc.edu>
2024-07-09 11:57:04 +0800

    error handler for stream

--- main.js
+++ main.js
@@ -16,17 +16,22 @@ const app = new Koa();
 const router = new Router();
 
 router.get("/stream", async (ctx) => {
-  const { trackid } = ctx.query;
-  if (trackid) {
-    const music = await Library.find({ trackid: trackid });
-    if (music[0]) {
-      const path = music[0].file;
-      ctx.set({ "Content-Type": "audio/mp3" });
-      ctx.status = 206;
-      ctx.body = fs.createReadStream(path);
-    } else {
-      ctx.body = "music not found";
+  try {
+    const { trackid } = ctx.query;
+    if (trackid) {
+      const music = await Library.find({ trackid: trackid });
+      if (music[0]) {
+        const path = music[0].file;
+        ctx.set({ "Content-Type": "audio/mp3" });
+        ctx.status = 206;
+        ctx.body = fs.createReadStream(path);
+      } else {
+        ctx.body = "music not found";
+      }
     }
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = "server error";
   }
 });
 
