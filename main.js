8a007edb1708a148a62b511dcb8e061408cc02dd
Legion <sli857@wisc.edu>
2024-07-09 11:23:13 +0800

    login signup finished, secret in db not hashed yet

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ import cors from "koa-cors";
 import bodyParser from "koa-bodyparser";
 import fs from "fs";
 import { Library, User } from "./models.js";
-
+import CryptoJS from "crypto-js";
 const port = process.argv[2] || 3000;
+
+function decrypt(str) {
+  const bytes = CryptoJS.AES.decrypt(str, "Google2024Summer");
+  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+}
 
 const app = new Koa();
 const router = new Router();
@@ -26,19 +31,54 @@ router.get("/stream", async (ctx) => {
 });
 
 router.post("/login", async (ctx) => {
-  console.log(ctx.request.body);
+  try {
+    const { name, secret } = ctx.request.body;
+
+    if (!name || !secret) {
+      ctx.status = 400;
+      ctx.body = { status: 1, msg: "Name and secret are required." };
+      return;
+    }
+
+    const decrypted = decrypt(secret);
+
+    const user = await User.findOne({ name: name, secret: decrypted }).exec();
+
+    console.log(user);
+    if (user) {
+      ctx.status = 200;
+      ctx.body = { status: 0, msg: "Success" };
+    } else {
+      ctx.status = 401;
+      ctx.body = { status: 1, msg: "Username or Password error." };
+    }
+  } catch (error) {
+    console.error(error);
+    ctx.status = 500;
+    ctx.body = { status: 1, msg: "Internal Server Error" };
+  }
 });
 
 router.post("/signup", async (ctx) => {
   try {
+    const { name, secret } = ctx.request.body;
+    if (!name || !secret) {
+      ctx.status = 400;
+      ctx.body = { status: 1, msg: "Name and secret are required." };
+      return;
+    }
+    const decrypted = decrypt(secret);
+
     const newUser = new User({
-      name: ctx.request.body.name,
-      secret: ctx.request.body.secret,
+      name: name,
+      secret: decrypted,
     });
 
     await newUser.save();
+    ctx.status = 200;
     ctx.body = { status: 0, msg: "Success" };
   } catch (err) {
+    ctx.status = 401;
     ctx.body = { status: 1, msg: "User Already Exists." };
     console.log(err);
   }
